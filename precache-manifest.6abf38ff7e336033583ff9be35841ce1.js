d6390d4fec9886f45c103d2e047db8e4210d85f1
Temi-t <temiemmanuel@yahoo.com>
2020-10-16 03:07:43 +0100

    Updates

---
+++ precache-manifest.6abf38ff7e336033583ff9be35841ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8091fe906647e937804d3360bf460b8e",
+    "url": "/chat-to-doo/index.html"
+  },
+  {
+    "revision": "d00ff4a4c3b19e07eed0",
+    "url": "/chat-to-doo/static/css/main.43df5b8d.chunk.css"
+  },
+  {
+    "revision": "994a66dfe9f7339c3466",
+    "url": "/chat-to-doo/static/js/2.f1d4f1ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-to-doo/static/js/2.f1d4f1ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00ff4a4c3b19e07eed0",
+    "url": "/chat-to-doo/static/js/main.a63d83e8.chunk.js"
+  },
+  {
+    "revision": "a784de6fa354cf9df3a8",
+    "url": "/chat-to-doo/static/js/runtime-main.0bbff82f.js"
+  }
+]);
